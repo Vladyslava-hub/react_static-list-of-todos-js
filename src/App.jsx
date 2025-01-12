95006b1e559939b4bcb50048e6e167eb8c91debd
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2025-01-12 18:15:00 +0000

    fixed task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import './App.scss';
-import { todos } from './api/todos.json';
+// eslint-disable-next-line import/named
 import { TodoList } from './components/TodoList';
+
+import todosFromServer from './api/todos.json';
+import usersFromServer from './api/users.json';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+export const todos = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
 
 export const App = () => (
   <div className="App">
--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
+// eslint-disable-next-line import/no-unresolved, import/extensions
 import { UserInfo } from '../UserInfo/UserInfo';
 
-export const TodoInfo = ({ todo }) => {
-  const { title, completed, user } = todo;
-
-  return (
-    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
-      <h2 className="TodoInfo__title">{title}</h2>
-      {user && <UserInfo user={user} />}
-    </article>
-  );
-};
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
 
 TodoInfo.propTypes = {
   todo: PropTypes.shape({
     title: PropTypes.string.isRequired,
     completed: PropTypes.bool.isRequired,
     user: PropTypes.shape({
+      id: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
       email: PropTypes.string.isRequired,
     }),
